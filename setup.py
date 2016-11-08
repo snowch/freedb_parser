0926a53fd2d96e7fbd201f4d6e9b013e91d5a754
Chris Snow <chsnow123@gmail.com>
2016-11-08 07:16:04 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name='freedb_parser',
+  version='0.1',
+  author='Chris Snow',
+  author_email='chsnow123@gmail.com',
+  url='https://github.com/snowch/freedb_parser/',
+  packages=['arubomu'],
+  install_requires=[],
+)
